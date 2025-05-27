e232ff805478aed240ccb8af97a02f21e30ea961
Keerthi Keerthi <keerthihan05@gmail.com>
2025-05-27 18:33:12 +0530

    Update: your custom message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import Header from './Components/Header/header.jsx'
-import About from './Components/About/about.jsx'
-import Services from './Components/Services/services.jsx'
-import Mywork from './Components/Mywork/mywork.jsx'
-import Contact from './Components/Contact/contact.jsx'
-import Footer from './Components/Footer/footer.jsx'
+import React from 'react';
+import Navbar from './Components/navbar/navbar.jsx';
+import Header from './Components/Header/header.jsx';
+import About from './Components/About/about.jsx';
+import Services from './Components/Services/services.jsx';
+import Mywork from './Components/Mywork/mywork.jsx';
+import Contact from './Components/Contact/contact.jsx';
+import Footer from './Components/Footer/footer.jsx';
+
 function App() {
   return (
     <div>
-      
+      <Navbar />
       <Header />
-      <About/>
-      <Services/>
-      <Mywork/>
-      <Contact/>
-      <Footer/>
+      <About />
+      <Services />
+      <Mywork />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
